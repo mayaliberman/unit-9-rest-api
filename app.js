5e18a62afea45da4266eca293006313adc26a17f
Maya Liberman <mayaliberman1@gmail.com>
2019-11-20 19:21:28 +0200

    lslsls

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 // load modules
 const express = require('express');
+console.log('@@@GIL hi!');
 const morgan = require('morgan');
 const { models, Course, User } = require('./db');
 // const Course = require('./db/models/course').Course;
@@ -9,11 +10,9 @@ const bcryptjs = require('bcryptjs');
 const auth = require('basic-auth');
 const { check, validationResult } = require('express-validator');
 
-
-
 // variable to enable global error logging
-const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
-
+const enableGlobalErrorLogging =
+  process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 //Async Handles to retreive data async
 function asyncHandler(cb) {
@@ -23,9 +22,9 @@ function asyncHandler(cb) {
     } catch (err) {
       next(err);
     }
-  }
+  };
 }
-//Authentication of user 
+//Authentication of user
 
 const authenicateUser = asyncHandler(async (req, res, next) => {
   let message = null;
@@ -33,38 +32,36 @@ const authenicateUser = asyncHandler(async (req, res, next) => {
   const credentials = auth(req);
 
   if (credentials) {
-
     const user = await models.User.findOne({
       where: {
         emailAddress: credentials.name
       }
     });
-    
+
     if (user) {
-      const authenticated = bcryptjs
-        .compareSync(credentials.pass, user.password);
+      const authenticated = bcryptjs.compareSync(
+        credentials.pass,
+        user.password
+      );
 
       if (authenticated) {
-
         req.currentUser = user;
-
       } else {
-        message = `Authentication failure for email: ${user.emailAddress}`
+        message = `Authentication failure for email: ${user.emailAddress}`;
       }
     } else {
-      message = `Authentication found for email ${credentials.emailAddress}`
+      message = `Authentication found for email ${credentials.emailAddress}`;
     }
   } else {
-    message = `Auth header not found`
+    message = `Auth header not found`;
   }
   if (message) {
     console.warn(message);
-    res.status(401).json({ message: `Access Denied` })
+    res.status(401).json({ message: `Access Denied` });
   } else {
     next();
   }
 });
-
 
 //user validation
 const userValidation = [
@@ -89,15 +86,13 @@ const userValidation = [
   //     }
   //   })
   // }),
-]
+];
 // create the Express app
 const app = express();
 // using middleware for reading json
 app.use(express.json());
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
-
-
 
 // TODO setup your api routes here
 
@@ -109,14 +104,13 @@ app.get('/api/users', authenicateUser, (req, res) => {
     lastName: user.lastName,
     emailAddress: user.emailAddress
   });
-
 });
 //CREATE NEW USER WITH VALIDATION
 app.post('/api/users', userValidation, (req, res) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const errorMessages = errors.array().map(error => error.msg);
-    res.status(400).json({ errors: errorMessages })
+    res.status(400).json({ errors: errorMessages });
   } else {
     //Hashing the new user password
     const user = req.body;
@@ -126,45 +120,46 @@ app.post('/api/users', userValidation, (req, res) => {
         res.location('/');
         res.status(201).end();
       })
-      .catch((err) => {
+      .catch(err => {
         throw err;
-      })
+      });
   }
 });
 
-
-
 //COURSES ROUTES
-app.get('/api/courses', asyncHandler(async (req, res) => {
-  const course = await models.Course.findAll();
-  res.json({course})
-  // const courses = await models.Course.findAll({ order: [['title', 'ASC']] });
-  
-  // if (courses) {
-  //   res.status(200).json(
-  //     {
-  //       message: 'courses',
-  //     }
-  //     // {
-  //     // title: courses.title,
-  //     // description: courses.description,
-  //     // }
-  //   )
-  // } else {
+app.get(
+  '/api/courses',
+  asyncHandler(async (req, res) => {
+    const course = await models.Course.findAll();
+    res.json({ course });
+    // const courses = await models.Course.findAll({ order: [['title', 'ASC']] });
 
-  // }
-}));
+    // if (courses) {
+    //   res.status(200).json(
+    //     {
+    //       message: 'courses',
+    //     }
+    //     // {
+    //     // title: courses.title,
+    //     // description: courses.description,
+    //     // }
+    //   )
+    // } else {
+
+    // }
+  })
+);
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to the REST API project!',
+    message: 'Welcome to the REST API project!'
   });
 });
 
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route Not Found',
+    message: 'Route Not Found'
   });
 });
 
@@ -176,7 +171,7 @@ app.use((err, req, res, next) => {
 
   res.status(err.status || 500).json({
     message: err.message,
-    error: {},
+    error: {}
   });
 });
 
--- db/models/course.js
+++ db/models/course.js
@@ -2,41 +2,43 @@
 
 const Sequelize = require('sequelize');
 
-module.exports = (sequelize) => {
-    class Course extends Sequelize.Model { }
-    Course.init({
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        title: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        description: {
-            type: Sequelize.TEXT,
-            allowNull: false,
-        },
-        estimatedTime: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-        mateiralsNeeded: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        }
-    }, { sequelize });
+module.exports = sequelize => {
+  class Course extends Sequelize.Model {}
+  Course.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      estimatedTime: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      materialsNeeded: {
+        type: Sequelize.STRING,
+        allowNull: true
+      }
+    },
+    { sequelize }
+  );
 
-    Course.associate = (models) => {
-        Course.belongsTo(models.User, {
-            // as: 'userId',
-            foreignKey: 
-            {
-                fieldName: 'userId',
-                allowNull: false,
-            }
-        })
-    }
-    return Course;
-}
+  Course.associate = models => {
+    Course.belongsTo(models.User, {
+      // as: 'userId',
+      foreignKey: {
+        fieldName: 'userId',
+        allowNull: false
+      }
+    });
+  };
+  return Course;
+};
